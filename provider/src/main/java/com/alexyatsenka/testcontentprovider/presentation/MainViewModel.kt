86b33a7d6b0938e4288100906376df0ee47ed6ee
Alex Yatsenka <aliaksei.yatsenka@innowise.com>
2024-10-25 10:07:22 +0200

    Error handling on MainViewModel (provider)

--- provider/src/main/java/com/alexyatsenka/testcontentprovider/presentation/MainViewModel.kt
+++ provider/src/main/java/com/alexyatsenka/testcontentprovider/presentation/MainViewModel.kt
@@ -1,11 +1,16 @@
 package com.alexyatsenka.testcontentprovider.presentation
 
 import androidx.lifecycle.ViewModel
+import com.alexyatsenka.models.domain.Note
 import com.alexyatsenka.testcontentprovider.domain.usecase.getNotes.GetNotesUseCase
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
     getNotesUseCase: GetNotesUseCase
 ) : ViewModel() {
-    val notes = getNotesUseCase().getOrThrow()
+    val notes = getNotesUseCase().let {
+        if(it.isSuccess) it.getOrThrow()
+        else flow { emptyList<Note>() }
+    }
 }
